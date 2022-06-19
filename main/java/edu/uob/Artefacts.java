9ffc94051a9e9cd9c31008a0e3d8e4107fa21f0e
BBQskmt <94111639+BBQskmt@users.noreply.github.com>
2022-06-19 17:54:18 +0100

    Delete main/java/edu/uob directory

--- main/java/edu/uob/Artefacts.java
+++
@@ -1,8 +0,0 @@
-package edu.uob;
-
-public class Artefacts extends GameEntity{
-
-    public Artefacts(String name, String description) {
-        super(name, description);
-    }
-}
--- main/java/edu/uob/Characters.java
+++
@@ -1,7 +0,0 @@
-package edu.uob;
-
-public class Characters extends GameEntity{
-    public Characters(String name, String description) {
-        super(name, description);
-    }
-}
